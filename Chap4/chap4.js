0e03732b4163dc4a728579539b245e7d2c5f3f79
SahilPatil <135830327+Coffee2003@users.noreply.github.com>
2023-08-03 15:43:21 +0530

    Create chap4.js

---
+++ Chap4/chap4.js
@@ -0,0 +1,13 @@
+const arr=[1,2,3,4,5];
+const arr1=[6,7,8,9];
+
+console.log(arr.length);
+
+arr.push("6");
+arr.pop();
+arr.shift();
+arr.unshift(0);
+console.log(arr.keys(3));
+console.log(arr.concat(arr1));
+console.log(arr.slice(0,2));
+
