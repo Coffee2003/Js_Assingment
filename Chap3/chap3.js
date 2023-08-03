629c21ea621637cc0e0f0b373a60c79252873087
SahilPatil <135830327+Coffee2003@users.noreply.github.com>
2023-08-03 15:42:37 +0530

    Create chap3.js

---
+++ Chap3/chap3.js
@@ -0,0 +1,14 @@
+let str="Sahil Patil   ";
+
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.charAt(1));
+console.log(str.substring(0,5));
+console.log(str.length);
+console.log(str.replace("Sahil","Rahil"));
+console.log(str.concat("is great"));
+console.log(str.endsWith("il"));
+console.log(str.startsWith("Sa"));
+console.log(str.trim());
+
+
