d2ee1edc088edca9f3f090555e36eb789585bbf4
SahilPatil <135830327+Coffee2003@users.noreply.github.com>
2023-08-03 15:44:50 +0530

    Create chap5.js

---
+++ Chap5/chap5.js
@@ -0,0 +1,18 @@
+// Q1
+var a=1;
+try{
+var x=a+b;
+}catch(e){
+console.log(e+"Error");
+}
+
+//Q2
+
+var a=1;
+try{
+var x=a+b;
+}catch(e){
+console.log(e+"Error");
+}finally{
+    console.log("Final block")
+}
