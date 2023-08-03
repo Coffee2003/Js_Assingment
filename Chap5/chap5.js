10a56c85711d52af2de3b2f6c612648cfc4e1f77
SahilPatil <135830327+Coffee2003@users.noreply.github.com>
2023-08-03 15:54:21 +0530

    Update chap5.js

--- Chap5/chap5.js
+++ Chap5/chap5.js
@@ -16,3 +16,31 @@ console.log(e+"Error");
 }finally{
     console.log("Final block")
 }
+
+//Q3
+num=15;
+
+if(num<=18){
+    throw new Error("age is not valid");
+}
+else{
+    console.log("Welcome to vote!");
+}
+
+
+// Q.4) Create a user defined exception :
+
+class InvalidNumber extends Error{
+    meth(){
+        try {
+            num=10;
+            if (num<20) {
+                throw new 
+            }
+        } catch (error) {
+            
+        }
+    }
+    
+    
+}
